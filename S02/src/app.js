c7365d1a4b247eae2f96ae6d04e34828918dd197
Olivier Mathieu-Grégoire <1847622@cstj.qc.ca>
2021-09-02 16:43:32 -0400

    fin du cours 2 septembre

--- S02/src/app.js
+++ S02/src/app.js
@@ -19,19 +19,52 @@ res.send('Notre premiere route avec express');
 
 });
 
-app.get('/somme',(req,res) => {
+// maths/somme
+//maths/difference
+//maths/produit
+//maths/quotient
+//maths/reste
 
-//console.log(req.query);
+// avec le :operation on prend le lien écrit par le client sur le url du fureteur pour ensuite faire un switch case pour rediriger ce qu'on veut faire
+app.get('/maths/:operation',(req,res) => {
+
+    const operation = req.params.operation;
+    console.log(operation);
 
 // parseInt permet de convertir un variable de type string en variable de type int pour pouvoir calculer 
 // Le deuxieme parametre ( 10 ) est la base des nombres 
 const a = parseInt(req.query.a,10);
 const b = parseInt(req.query.b,10);
+let resultat = 0;
 
-res.status(200);
-res.set('Content-Type','text/html');
+switch(operation)
+{
+    case 'somme':
+        resultat = a+b;
+        break;
+    case 'difference':
+        resultat = a - b;
+         break;
+     case 'produit':
+         resultat = a * b;
+            break;
+    case 'quotient':
+        resultat = a / b;
+        break;
+        case 'reste':
+            resultat = a % b;
+        break;
+    default: 
+    res.status(400);
+    return res.send('Opération incconue');
 
-res.send(`<h1>${a+b}</h1>`);
+}
+
+        res.status(200);
+        res.set('Content-Type','text/html'); // Voici le contenu qu'on va envoyer 
+        res.send(`<h1>${resultat}</h1>`); // send est équivalent a un return 
+
+//console.log(req.query);
 
 });
 
